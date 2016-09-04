6d3e7d830d5c2cb36181053b83dcc6a31fba939f
Hai Phan Nguyen <pnghai@gmail.com>
2016-09-04 20:34:29 +0700

    add searchform

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
-<div class="grid" data-masonry='{ "itemSelector": ".post", "columnWidth": ".grid-sizer" , "percentPosition": "true"}' >
+<div class="grid" data-masonry='{ "itemSelector": ".post", "columnWidth": ".grid-sizer" , "percentPosition": "true", "stamp":".home-sidebar"}' >
   <div class="grid-sizer"></div>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,24 @@
 <header class="banner">
   <div class="container">
     <div class="head-wrapper row">
-      <div class="col-xs-12">
+      <div class="logo">
         <a class="brand" href="<?= esc_url(home_url('/')); ?>" alt="<?php bloginfo('name'); ?>"><img src="http://dummyimage.com/184x37/009c1a/fff.jpg"></a>
-        <nav class="nav-primary">
-          <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-          endif;
-          ?>
-        </nav>
       </div>
+      <nav class="nav-primary">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?>
+      </nav>
+      <nav class="right-nav">
+        <ul class="wrapper list-inline">
+          <li><a href="#">Video</a></li>
+          <li>
+            <?php echo get_search_form();?>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </header>
