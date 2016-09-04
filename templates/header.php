d85a93618ea220ea2b4f784de841bab098737cbd
Hai Phan Nguyen <pnghai@gmail.com>
2016-09-04 18:09:58 +0700

    fix header spacing

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,16 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="head-wrapper row">
+      <div class="col-xs-12">
+        <a class="brand" href="<?= esc_url(home_url('/')); ?>" alt="<?php bloginfo('name'); ?>"><img src="http://dummyimage.com/184x37/009c1a/fff.jpg"></a>
+        <nav class="nav-primary">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
+          ?>
+        </nav>
+      </div>
+    </div>
   </div>
 </header>
